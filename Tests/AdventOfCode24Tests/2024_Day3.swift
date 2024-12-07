265305ecd0450ca08f0feb47e1e1b70d91d2df9f
Christoffer Winterkvist <christoffer@winterkvist.com>
2024-12-07 16:48:22 +0100

    Advent of Code 2024 Day 3 solved

---
+++ Tests/AdventOfCode24Tests/2024_Day3.swift
@@ -0,0 +1,128 @@
+@testable import AdventOfCode2024
+import RegexBuilder
+import Testing
+
+/*
+ --- Day 3: Mull It Over ---
+
+ "Our computers are having issues, so I have no idea if we have any Chief Historians in stock! You're welcome to check the warehouse, though," says the mildly flustered shopkeeper at the North Pole Toboggan Rental Shop. The Historians head out to take a look.
+
+ The shopkeeper turns to you. "Any chance you can see why our computers are having issues again?"
+
+ The computer appears to be trying to run a program, but its memory (your puzzle input) is corrupted. All of the instructions have been jumbled up!
+
+ It seems like the goal of the program is just to multiply some numbers. It does that with instructions like mul(X,Y), where X and Y are each 1-3 digit numbers. For instance, mul(44,46) multiplies 44 by 46 to get a result of 2024. Similarly, mul(123,4) would multiply 123 by 4.
+
+ However, because the program's memory has been corrupted, there are also many invalid characters that should be ignored, even if they look like part of a mul instruction. Sequences like mul(4*, mul(6,9!, ?(12,34), or mul ( 2 , 4 ) do nothing.
+
+ For example, consider the following section of corrupted memory:
+
+ xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+ Only the four highlighted sections are real mul instructions. Adding up the result of each instruction produces 161 (2*4 + 5*5 + 11*8 + 8*5).
+
+ Scan the corrupted memory for uncorrupted mul instructions. What do you get if you add up all of the results of the multiplications?
+ */
+
+fileprivate func part1(_ file: String) throws -> Int {
+    let contents = try readFile(file)
+    let regex = Regex {
+        "mul("
+        Capture { OneOrMore(.digit) } transform: { Int($0) }
+        ","
+        Capture { OneOrMore(.digit) } transform: { Int($0) }
+        ")"
+    }
+
+    let result = contents
+        .matches(of: regex)
+        .reduce(into: 0) { partialResult, match in
+            let (_, x, y) = match.output
+            if let x, let y {
+                partialResult = partialResult + x * y
+            }
+        }
+    return result
+}
+
+@Test("https://adventofcode.com/2024/day/3")
+func AdventOfCode_2024_03_example_part1() throws {
+    let result = try part1("2024/03/example.txt")
+    #expect(result == 161)
+}
+
+@Test
+func AdventOfCode_2024_03_part1() throws {
+    let result = try part1("2024/03/input.txt")
+    #expect(result == 182780583)
+}
+
+/*
+ --- Part Two ---
+
+ As you scan through the corrupted memory, you notice that some of the conditional statements are also still intact. If you handle some of the uncorrupted conditional statements in the program, you might be able to get an even more accurate result.
+
+ There are two new instructions you'll need to handle:
+
+ The do() instruction enables future mul instructions.
+ The don't() instruction disables future mul instructions.
+ Only the most recent do() or don't() instruction applies. At the beginning of the program, mul instructions are enabled.
+
+ For example:
+
+ xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+ This corrupted memory is similar to the example from before, but this time the mul(5,5) and mul(11,8) instructions are disabled because there is a don't() instruction before them. The other mul instructions function normally, including the one at the end that gets re-enabled by a do() instruction.
+
+ This time, the sum of the results is 48 (2*4 + 8*5).
+
+ Handle the new instructions; what do you get if you add up all of the results of just the enabled multiplications?
+ */
+
+fileprivate func part2(_ file: String) throws -> Int {
+    let contents = try readFile(file)
+    var splits = contents.components(separatedBy: "don't()")
+    let first = splits.removeFirst()
+
+    var validStrings = [first]
+
+    for split in splits {
+        var subSplit = split.components(separatedBy: "do()")
+        subSplit.removeFirst()
+        subSplit.forEach { validStrings.append($0) }
+    }
+
+    let regex = Regex {
+        "mul("
+        Capture { OneOrMore(.digit) } transform: { Int($0) }
+        ","
+        Capture { OneOrMore(.digit) } transform: { Int($0) }
+        ")"
+    }
+
+    var result: Int = 0
+    for string in validStrings {
+        _ = string
+            .matches(of: regex)
+            .reduce(result) { partialResult, match in
+                let (_, x, y) = match.output
+                if let x, let y {
+                    result = partialResult + x * y
+                }
+                return result
+            }
+    }
+
+    return result
+}
+
+@Test
+func AdventOfCode_2024_03_example_part2() throws {
+    let result = try part2("2024/03/example2.txt")
+    #expect(result == 48)
+}
+
+@Test
+func AdventOfCode_2024_03_part2() throws {
+    let result = try part2("2024/03/input.txt")
+    #expect(result == 90772405)
+}
+
